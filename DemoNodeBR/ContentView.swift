cc8b9129f53e5e0244b0a0ce5ccce33734846f82
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-11-18 20:46:43 -0300

    Initial Commit

---
+++ DemoNodeBR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoNodeBR
+//
+//  Created by Lucas Dal Pra Brascher on 18/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoNodeBR/DemoNodeBRApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoNodeBRApp.swift
+//  DemoNodeBR
+//
+//  Created by Lucas Dal Pra Brascher on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoNodeBRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
